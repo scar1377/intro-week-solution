74eb21b8095d697191c0c0f95c43c60f478c0643
Mitch Lloyd <mitch@northcoders.com>
2020-04-21 10:56:53 +0100

    add first draft of arrays

---
+++ day_3/3_arrays/challenges.js
@@ -0,0 +1,29 @@
+
+function getLastItem(array) {
+
+}
+// should return the last item from an array
+
+function getLastItems(array, n) {
+  // return an array with the last n items of the array
+}
+
+
+function removeItem(array, n) {
+  // return a new array without the item on position 'n', effectively removing it from the array
+}
+
+
+function mergeArrays(arr1, arr2) {
+  // return a new array with all the elements of arr1 followed by all the elements of arr2
+}
+
+
+function getSandwichFilling(sandwich) {
+  // if an array is like a sandwich, the first and last items are the bread, return an array with the filling of the sandwich
+}
+
+function accessItem(array,index) {
+
+  // should access an array element at a specified index position
+}
---
+++ day_3/3_arrays/practice.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+const multiTypeArray = ['I am a string', 42, true, [1, 2, 3]];
+
+console.log('checking multiTypeArray');
+checkThat(typeof alphaSample[0]).isEqualTo(FILL_ME_IN);
+checkThat(typeof alphaSample[1]).isEqualTo(FILL_ME_IN);
+checkThat(typeof alphaSample[2]).isEqualTo(FILL_ME_IN);
+checkThat(typeof alphaSample[3]).isEqualTo(FILL_ME_IN);
+
+
+console.log('checking alphaSample');
+
+const alphaSample = ['a','b','c'];
+
+alphaSample.push('d');
+alphaSample.push('g');
+
+checkThat(alphaSample).isEqualTo(FILL_ME_IN);
+
+const lastItem = alphaSample.pop();
+
+checkThat(alphaSample).isEqualTo(FILL_ME_IN);
+checkThat(lastItem).isEqualTo(FILL_ME_IN);
