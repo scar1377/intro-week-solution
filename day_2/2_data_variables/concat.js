01f0fab28c0f835f71eda6938c65835fe1d1f397
Mitch Lloyd <mitch@northcoders.com>
2020-04-23 17:54:49 +0100

    add some notes on variables

--- day_2/2_data_variables/concat.js
+++ day_2/2_data_variables/concat.js
@@ -1,34 +1,16 @@
-
-
-const item0 = 'apples';
-const item1 = 'bananas';
-const item2 = 'pears';
+const item0 = "apples";
+const item1 = "bananas";
+const item2 = "pears";
 
 let shoppingList = item0.concat(FILL_ME_IN);
 
 // use the additional item variables from above
 // checkThat(shoppingList).isEqualTo('apples, bananas and pears');
 
-
-const filePath1 = '/Users/mitch/northcoders/remote_precourse/day_1/1_intro_strings';
+const filePath1 = "/Users/mitch/northcoders/remote_precourse/day_1/1_intro_strings";
 
 // find the index position of the last / forward slash
 // use a string method to find the slice of the string at that position
-// check that a value can 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// check that a value can
 
 var FILL_ME_IN;
