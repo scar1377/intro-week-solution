2bc47bde05563c0fc394a95d1b774d435e3ec38f
Mitch Lloyd <mitch@northcoders.com>
2020-04-23 15:19:39 +0100

    add more assertions for the object challenges

--- day_3/3_objects/challenges.js
+++ day_3/3_objects/challenges.js
@@ -19,25 +19,85 @@ try {
 function checkIfPropertyExists(object, key) {}
 // This function should take an object and a key and return boolean that indicates whether or not the object has the given keys
 
+try {
+  check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "name").returns(true);
+  check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "age").returns(true);
+  check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "pets").returns(false);
+
+  printGreenMessage("Pass :)");
+} catch (error) {
+  printRedMessage(error);
+}
+
 function createObject(keyValuePair) {}
 // This function should take a key value pair (stored in an array) and use it to create an object with a key and a value
+
+try {
+  check(createObject).whenCalledWith(["name", "shaq"]).returns({ name: "shaq" });
+  check(createObject).whenCalledWith(["fruit", "apple"]).returns({ fruit: "apple" });
+  check(createObject).whenCalledWith(["language", "haskell"]).returns({ language: "haskell" });
+
+  printGreenMessage("Pass :)");
+} catch (error) {
+  printRedMessage(error);
+}
 
 function countProperties(obj) {}
 // This function should take an object and count the number of properties it has
 
+try {
+  check(countProperties).whenCalledWith({}).returns(0);
+  check(countProperties).whenCalledWith({ name: "shaq" }).returns(1);
+  check(countProperties).whenCalledWith({ name: "shaq", job: "tutor", city: "Manchester" }).returns(3);
+
+  printGreenMessage("Pass :)");
+} catch (error) {
+  printRedMessage(error);
+}
+
 function updateVoterAddress(voter, correctHouseNumber) {
-  /* Uh-Oh! We've got some silly voters who've registered their addresses incorrectly. Lets help them fix those typos by changing their houseNumber to the correctHouseNumber. The user object looks like this:
+  /* Uh-Oh! We've got some silly voters who've registered their addresses incorrectly.
+   Lets help them fix those typos by changing their houseNumber to the correctHouseNumber.
+   The user object looks like this:
     {
       name: "Alex",
       age: 39,
-      address: { 
-        houseNumber: 2, 
-        street: "Old St", 
-        city: "Chester" 
+      address: {
+        houseNumber: 2,
+        street: "Old St",
+        city: "Chester"
       }
     };
     Note - The function does NOT need to return anything.
   */
+}
+
+try {
+  const voter = {
+    name: "Alex",
+    age: 39,
+    address: {
+      houseNumber: 2,
+      street: "Old St",
+      city: "Chester",
+    },
+  };
+  console.log(`updateVoterAddress() updates the voter's houseNumber`);
+  updateVoterAddress(voter, 10);
+
+  check(voter).isEqualTo({
+    name: "Alex",
+    age: 39,
+    address: {
+      houseNumber: 10,
+      street: "Old St",
+      city: "Chester",
+    },
+  });
+
+  printGreenMessage("Pass :)");
+} catch (error) {
+  printRedMessage(error);
 }
 
 function createUserString(userObj) {
@@ -45,6 +105,20 @@ function createUserString(userObj) {
   // returns a string with the user's details in the form:
   // 'name: Mitch, age: 27, language: Javascript';
   // Note - this is a good use case of string template literals.
+}
+
+try {
+  check(createUserString)
+    .whenCalledWith({ name: "Mitch", age: 30, language: "Javascript" })
+    .returns("name: Mitch, age: 27, language: Javascript");
+
+  check(createUserString)
+    .whenCalledWith({ name: "Anat", age: 24, language: "Ruby" })
+    .returns("name: Mitch, age: 27, language: Javascript");
+
+  printGreenMessage("Pass :)");
+} catch (error) {
+  printRedMessage(error);
 }
 
 function updateCoinMachine(coinMachine, money) {
@@ -64,6 +138,25 @@ function check(func) {
       this.args = args;
       return this;
     },
+    isEqualTo(expected) {
+      const { actual } = this;
+      console.log(expected, "expected");
+      console.log(actual, "actual");
+
+      if (typeof actual === "object" && typeof expected === "object") {
+        console.log("here");
+
+        if (!checkDeeplyEqual(actual, expected)) {
+          throw new Error(
+            `${JSON.stringify(actual, null, 2)}\n is not equal to the expected value of \n${JSON.stringify(
+              expected,
+              null,
+              2
+            )}`
+          );
+        }
+      } else if (actual !== expected) throw new Error(`${actual} is not equal to the expected value of ${expected}`);
+    },
     returns(expected) {
       const actual = this.func(...this.args);
       if (typeof actual === "object" && typeof expected === "object") {
@@ -74,30 +167,33 @@ function check(func) {
     },
   };
   const obj = Object.create(methods);
-  obj.func = func;
+  if (typeof func === "function") obj.func = func;
+  else obj.actual = func;
   return obj;
 }
 
-function checkDeeplyEqual(actualList, expectedList) {
-  if (typeof actualList !== typeof expectedList) return false;
-  if (actualList.length !== expectedList.length) return false;
-
-  for (let i = 0; i < actualList.length; i++) {
-    if (Array.isArray(actualList[i]) && Array.isArray(expectedList[i])) {
-      if (!checkDeeplyEqual(actualList[i], expectedList[i])) return false;
-    } else {
-      if (actualList[i] !== expectedList[i]) return false;
-    }
-  }
-
-  return true;
+function checkDeeplyEqual(coll1, coll2) {
+  let areEqual = true;
+  if (typeof coll1 === "object" && typeof coll2 === "object") {
+    if (Object.keys(coll1).length !== Object.keys(coll2).length) return false;
+    if (Array.isArray(coll1) === Array.isArray(coll2)) {
+      for (let key1 in coll1) {
+        if (!coll2[key1]) return false;
+        else areEqual = checkDeeplyEqual(coll1[key1], coll2[key1]);
+        if (!areEqual) return false;
+      }
+    } else return false;
+  } else return coll1 === coll2;
+  return areEqual;
 }
 
 function createFeedBackString(item) {
   const lookup = {
     string: (item) => `"${item}"`,
-    object: (item) => JSON.stringify(item),
+    object: (item) => JSON.stringify(item, null, 3),
     undefined: (x) => x,
+    boolean: (x) => x,
+    number: (x) => x,
   };
   return lookup[typeof item](item);
 }
