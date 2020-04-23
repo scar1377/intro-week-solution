73d57ea4ae0bbe74d1e95a49d31bd08a13438658
Mitch Lloyd <mitch@northcoders.com>
2020-04-23 13:14:57 +0100

    rename folder to 2_arrays

--- day_3/2_arrays/practice.js
+++ day_3/2_arrays/practice.js
@@ -1,25 +1,17 @@
+const multiTypeArray = ["I am a string", 42, true, [1, 2, 3]];
 
-
-
-
-
-
-
-const multiTypeArray = ['I am a string', 42, true, [1, 2, 3]];
-
-console.log('checking multiTypeArray');
+console.log("checking multiTypeArray");
 checkThat(typeof alphaSample[0]).isEqualTo(FILL_ME_IN);
 checkThat(typeof alphaSample[1]).isEqualTo(FILL_ME_IN);
 checkThat(typeof alphaSample[2]).isEqualTo(FILL_ME_IN);
 checkThat(typeof alphaSample[3]).isEqualTo(FILL_ME_IN);
 
+console.log("checking alphaSample");
 
-console.log('checking alphaSample');
+const alphaSample = ["a", "b", "c"];
 
-const alphaSample = ['a','b','c'];
-
-alphaSample.push('d');
-alphaSample.push('g');
+alphaSample.push("d");
+alphaSample.push("g");
 
 checkThat(alphaSample).isEqualTo(FILL_ME_IN);
 
--- day_3/3_objects/challenges.js
+++ day_3/3_objects/challenges.js
@@ -1,34 +1,21 @@
-function makeCard(value,suit) {};
+function makeCard(value, suit) {}
 // This function should return a new object with a value property and a suit property - the function will be passed a value and a suit for the property values
 
-function isFaceCard(card) {
+function isFaceCard(card) {}
+// This function should take a card object with a value and suit property and
 
-}
-// This function should take a card object with a value and suit property and 
+checkThat(isFaceCard({ value: "Q", suit: "spade" })).isEqualTo(true);
 
-
-checkThat(isFaceCard({ "value" : 'Q', suit: 'spade' })).isEqualTo(true);
-
-
-
-function accessObject(object,key) {
-
-}
+function accessObject(object, key) {}
 // This function should take an object and a key and return the object's property value;
 
-function checkIfPropertyExists(object,key) {
-
-}
+function checkIfPropertyExists(object, key) {}
 // This function should take an object and a key and return boolean that indicates whether or not the object has the given keys
 
-function createObject(keyValuePair) {
-
-}
+function createObject(keyValuePair) {}
 // This function should take a key value pair (stored in an array) and use it to create an object with a key and a value
 
-function countProperties(obj) {
-
-}
+function countProperties(obj) {}
 // This function should take an object and count the number of properties it has
 
 function updateVoterAddress(voter, correctHouseNumber) {
@@ -53,12 +40,11 @@ function createUserString(userObj) {
   // Note - this is a good use case of string template literals.
 }
 
-function updateCoinMachine(coinMachine,money) {
-  // should take a coinMachine object with the following properties: 
-
+function updateCoinMachine(coinMachine, money) {
+  // should take a coinMachine object with the following properties
   //    {
   //      '1p' : [],
   //      '2p': [],
-  //    } 
-  // should update the 
+  //    }
+  // should update the
 }
