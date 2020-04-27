7c02b32b968552bd3ebb53817be7dfa91c3a55eb
Mitch Lloyd <mitch@northcoders.com>
2020-04-27 17:21:03 +0100

    add variables challenge assertions for concat.js

--- day_2/2_data_variables/concat.js
+++ day_2/2_data_variables/concat.js
@@ -2,15 +2,107 @@ const item0 = "apples";
 const item1 = "bananas";
 const item2 = "pears";
 
-let shoppingList = item0.concat(FILL_ME_IN);
+// use the additional item variables from above to create a variable called shoppingList
 
-// use the additional item variables from above
-// checkThat(shoppingList).isEqualTo('apples, bananas and pears');
+let shoppingList = item0 + ", " + item1 + " and " + item2;
 
-const filePath1 = "/Users/mitch/northcoders/remote_precourse/day_1/1_intro_strings";
+console.log("shoppingList is declared and initialised as a list with values from items 0,1 and 2");
+try {
+  check(shoppingList).isEqualTo("apples, bananas and pears");
+  printGreenMessage("Success :)");
+} catch (e) {
+  printRedMessage(e);
+}
 
-// find the index position of the last / forward slash
+const filePath = "/Users/mitch/northcoders/remote_precourse/day_1/1_intro_strings";
+
+// find the index position of the last / forward slash in the filePath string
+let lastFowardSlashIndex = filePath1.lastIndexOf("/");
+let currentDirectory = filePath1.slice(lastFowardSlashIndex + 1);
 // use a string method to find the slice of the string at that position
 // check that a value can
 
+console.log("currentDirectory is intialised with the name of the directory and the end of the filePath");
+try {
+  check(currentDirectory).isEqualTo("1_intro_strings");
+  printGreenMessage("Success :)");
+} catch (e) {
+  printRedMessage(e);
+}
+
+// >>>>>>>>>>> DON'T ALTER ANYTHING BELOW THIS LINE <<<<<<<<<<<<<<<
+
 var FILL_ME_IN;
+
+function check(func) {
+  const methods = {
+    whenCalledWith(...args) {
+      this.args = args;
+      return this;
+    },
+    isEqualTo(expected) {
+      const { actual } = this;
+
+      if (typeof actual === "object" && typeof expected === "object") {
+        if (!checkDeeplyEqual(actual, expected)) {
+          throw new Error(
+            `${JSON.stringify(actual)}\n is not equal to the expected value of \n${JSON.stringify(expected)}`
+          );
+        }
+      } else if (actual !== expected) throw new Error(`${actual} is not equal to the expected value of ${expected}`);
+    },
+    returns(expected) {
+      const actual = this.func(...this.args);
+      if (typeof actual === "object" && typeof expected === "object") {
+        if (!checkDeeplyEqual(actual, expected)) {
+          throw new Error(createFeedback(this.func.name, actual, expected));
+        }
+      } else if (actual !== expected) throw new Error(createFeedback(this.func.name, actual, expected));
+    },
+  };
+  const obj = Object.create(methods);
+  if (typeof func === "function") obj.func = func;
+  else obj.actual = func;
+  return obj;
+}
+
+function checkDeeplyEqual(coll1, coll2) {
+  if (typeof coll1 !== "object" || typeof coll2 !== "object") return coll1 === coll2;
+
+  if (Object.keys(coll1).length !== Object.keys(coll2).length) return false;
+
+  if (Array.isArray(coll1) !== Array.isArray(coll2)) return false;
+
+  for (let key1 in coll1) {
+    if (!(key1 in coll2)) return false;
+    if (!checkDeeplyEqual(coll1[key1], coll2[key1])) return false;
+  }
+  return true;
+}
+
+function createFeedBackString(item) {
+  const lookup = {
+    string: (item) => `"${item}"`,
+    object: (item) => JSON.stringify(item),
+    undefined: (x) => x,
+    boolean: (x) => x,
+    number: (x) => x,
+  };
+  return lookup[typeof item](item);
+}
+
+function createFeedback(name, actual, expected) {
+  const actualString = createFeedBackString(actual);
+  const expectedString = createFeedBackString(expected);
+
+  const feedback = `${name}'s output was ${actualString}, but it should be ${expectedString}`;
+  return feedback;
+}
+
+function printRedMessage(message) {
+  console.log("\x1b[31m", message, "\x1b[0m");
+}
+
+function printGreenMessage(message) {
+  console.log("\x1b[32m", message, "\x1b[0m");
+}
