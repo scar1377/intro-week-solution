e1aa8ae3438a74babc717345b871849baadcdf75
Mitch Lloyd <mitch@northcoders.com>
2020-05-03 12:48:23 +0100

    remove old logic challenges

--- day-3/conditional_logic/challenges/0.js
+++
@@ -1,9 +0,0 @@
-function isBiggerThan10(num) {
-  if (num > 10) {
-    console.log("Your number is bigger than 10");
-  } else {
-    console.log("Your number is less than or equal to 10");
-  }
-}
-
-// assert that this function returns the correct message
