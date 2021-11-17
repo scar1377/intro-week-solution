0e097ecc0109f03ac3f412c2ee41ff9ae7ec1d97
Dave Ridealgh <davecodingstuff@gmail.com>
2021-11-17 18:53:08 +0000

    add function declarations to object challenges

--- 7-objects/challenges/index.js
+++ 7-objects/challenges/index.js
@@ -9,14 +9,18 @@ Take a look at the tests to see what each function should be returning given var
 When you're ready to move on to the next function replace skipTest with runTest.
 */
 
-// accessObject() should take an object and a key and return the object's property value
+function accessObject(obj, key) {
+  // This function should take an object and a key as its arguments and return the value found at the provided key in the input object
+}
 
 runTest("accessObject() can access a property value using a key", function () {
   check(accessObject).whenCalledWith({ name: "jonny", age: 32 }, "name").returns("jonny");
   check(accessObject).whenCalledWith({ name: "jonny", age: 32 }, "age").returns(32);
 });
 
-// checkIfPropertyExists() should take an object and a key and return a boolean that indicates whether or not the object has the given keys
+function checkIfPropertyExists(obj, key) {
+  // This function should take an object and a key as its arguments and return true if the input object contains the provided key and false otherwise
+}
 
 skipTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
   check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "name").returns(true);
@@ -24,7 +28,9 @@ skipTest("checkIfPropertyExists() checks if a property exists inside an object",
   check(checkIfPropertyExists).whenCalledWith({ name: "jonny", age: 32 }, "pets").returns(false);
 });
 
-// createObject() should take a key-value pair (stored in an array) and use it to create an object with a key and a value
+function createObject(arr) {
+  // This function should take an array consisting of two elements representing a key/ value pair as its argument and return an object with a single property based on the input
+}
 
 skipTest("createObject() creates a new object from a key value pair", function () {
   check(createObject).whenCalledWith(["name", "shaq"]).returns({ name: "shaq" });
@@ -32,7 +38,9 @@ skipTest("createObject() creates a new object from a key value pair", function (
   check(createObject).whenCalledWith(["language", "haskell"]).returns({ language: "haskell" });
 });
 
-// countProperties() should take an object and count the number of properties it has
+function countProperties(obj) {
+  // This function should take an object as its argument and return the total number of properties it contains
+}
 
 skipTest("countProperties() counts the number of key-value pairs for a given object", function () {
   check(countProperties).whenCalledWith({}).returns(0);
@@ -40,30 +48,35 @@ skipTest("countProperties() counts the number of key-value pairs for a given obj
   check(countProperties).whenCalledWith({ name: "shaq", job: "tutor", city: "Manchester" }).returns(3);
 });
 
-// createArrow() will take a direction string: "left", "right", "up" and "down" and return a corresponding arrow
-// Think of how you can use an object to solve this problem
+function createArrow(direction) {
+  // This function should take a string representing a direction ("left", "right", "up" or "down") as its argument and return the corresponding arrow ("←", "→", "↑", "↓")
+  // You don't need to utilise an object here, but think about how you could do so
+}
 
 skipTest("createArrow() will return an arrow pointing in the right direction", function () {
-  check(createArrow).whenCalledWith("right").returns("→");
   check(createArrow).whenCalledWith("left").returns("←");
+  check(createArrow).whenCalledWith("right").returns("→");
   check(createArrow).whenCalledWith("up").returns("↑");
   check(createArrow).whenCalledWith("down").returns("↓");
 });
 
-// Uh-Oh! We've got some silly voters who've registered their addresses incorrectly.
-//   Lets help them fix those typos by changing their houseNumber to the correctHouseNumber.
-//   The user object looks like this:
-//    {
-//      name: "Alex",
-//      age: 39,
-//      address: {
-//        houseNumber: 2,
-//        street: "Old St",
-//        city: "Chester"
-//      }
-//    };
-//    Note - The function does NOT need to return anything.
-//
+function updateVoterAddress(voter, correctHouseNumber) {
+  /*
+  This function should take an object representing a voter's details and a house number as its arguments
+  A voter object will take this form:
+  {
+    name: "Alex",
+    age: 39,
+    address: {
+      houseNumber: 2,
+      street: "Old St",
+      city: "Chester"
+    }
+  }
+  The voter's house number is incorrect and you should update it to be the one provided
+  This function does NOT need to return anything
+  */
+}
 
 skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
   const voter = {
@@ -89,12 +102,21 @@ skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
   });
 });
 
-// createUserString() should take as an argument an object with the format from createNorthcoder
-// returns a string with the user's details in the form:
-// "name: Mitch, age: 27, language: Javascript";
-
-// Note - this is a good use case of template literals:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+function createUserString(user) {
+  /*
+  This function should take an object representing a user's details as its argument
+  A user object will take this form:
+  {
+    name: "Mitch",
+    age: 30,
+    language: "JavaScript"
+  }
+  You should return a string consisting of these details in this form:
+  "name: Mitch, age: 27, language: Javascript"
+  This is a good use case for String Template Literals:
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+  */
+}
 
 skipTest("createUserString() will create a message from an details object", function () {
   check(createUserString)
@@ -106,14 +128,19 @@ skipTest("createUserString() will create a message from an details object", func
     .returns("name: Anat, age: 24, language: Ruby");
 });
 
-// updateCoinMachine() should take a coinMachine object with the following form:
-//  {
-//    '1p': 0,
-//    '2p': 0,
-//    '5p': 0,
-//    '10p: 0
-//  }
-// and should update the coinMachine to reflect any change that is added into the machine
+function updateCoinMachine(coinMachine, coin) {
+  /*
+  This function should take an object representing a coin machine and a string representing a coin as its arguments
+  A coinMachine object will take this form:
+  {
+    "1p": 0,
+    "2p": 0,
+    "5p": 0,
+    "10p": 0
+  }
+  You should 'add the provided coin to the machine' by altering the associated property and return the updated coinMachine
+  */
+}
 
 skipTest("updateCoinMachine() will create a message from an details object", function () {
   check(updateCoinMachine)
