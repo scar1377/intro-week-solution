8e96ef70684cf70e85a4f32e6372a52345a90c66
Dave Ridealgh <davecodingstuff@gmail.com>
2021-12-08 11:04:41 +0000

    remove redundant required functions in function challenges

--- 3-functions/challenges/index.js
+++ 3-functions/challenges/index.js
@@ -1,4 +1,4 @@
-const { check, runTest, skipTest, printGreenMessage, printRedMessage } = require("../../test-api");
+const { check, runTest, skipTest } = require("../../test-api");
 
 /*
 Instructions
