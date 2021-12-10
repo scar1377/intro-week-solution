69713ed8edbd1b9fc557337ca8ce477599463803
Dave Ridealgh <davecodingstuff@gmail.com>
2021-12-10 10:21:09 +0000

    whenCalledWith -> isEqualTo in array-methods exercises

--- 8-array-methods/exercises/part-2.js
+++ 8-array-methods/exercises/part-2.js
@@ -1,61 +1,133 @@
 const { check, runTest, skipTest } = require("../../test-api");
 
-// Once you have passed the current test, change skipTest on the following test to runTest so you are able to run it with Node
+/*
+Instructions
 
-runTest("makeAllUpperCase() can get all the strings from an array", function () {
-  check(makeAllUpperCase).whenCalledWith(["a", "b", "c"]).returns(["A", "B", "C"]);
-  check(makeAllUpperCase).whenCalledWith(["I", "love", "coding"]).returns(["I", "LOVE", "CODING"]);
+For these challenges you are expected to declare your own function with the name and suggested behaviour provided.
+Run this file with Node to run the tests and check your function works correctly.
+Take a look at the tests to see what each function should be returning given various arguments.
+When you're ready to move on to the next function replace skipTest with runTest.
+Remember to use an appropriate array method in your implementation.
+*/
+
+/*
+makeAllUpperCase()
+
+This function should take an array of strings as an argument, and return an array of those strings capitalised
+*/
+
+runTest("makeAllUpperCase() returns an array of capitalised strings", function () {
+  check(makeAllUpperCase(["a", "b", "c"])).isEqualTo(["A", "B", "C"]);
+  check(makeAllUpperCase(["I", "love", "coding"])).isEqualTo(["I", "LOVE", "CODING"]);
 });
+
+/*
+collectStrings()
+
+This function should take an array containing various data types as an argument, and return an array of the strings found in the original (retaining the order)
+
+*/
 
 skipTest("collectStrings() can get all the strings from an array", function () {
-  check(collectStrings).whenCalledWith(["a", "b", "c"]).returns(["a", "b", "c"]);
-  check(collectStrings).whenCalledWith(["a", 10, "b", 1000, "c"]).returns(["a", "b", "c"]);
+  check(collectStrings(["a", "b", "c"])).isEqualTo(["a", "b", "c"]);
+  check(collectStrings(["a", 10, "b", 1000, "c"])).isEqualTo(["a", "b", "c"]);
+  check(collectStrings([true, "hello", 45, "there", false])).isEqualTo(["hello", "there"]);
+  check(collectStrings([1, 2, 3, 4, 5])).isEqualTo([]);
 });
+
+/*
+getEvenNumbers()
+
+This function should take an array of numbers as an argument, and return an array of only the even numbers
+*/
 
 skipTest("getEvenNumbers() can get all the even numbers from an array of numbers", function () {
-  check(getEvenNumbers).whenCalledWith([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).returns([2, 4, 6, 8, 10]);
-  check(getEvenNumbers).whenCalledWith([9, 100, 13, 20]).returns([100, 20]);
+  check(getEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).isEqualTo([2, 4, 6, 8, 10]);
+  check(getEvenNumbers([9, 100, 13, 20])).isEqualTo([100, 20]);
 });
+
+/*
+collectPlurals()
+
+This function should take an array of strings as an argument, and return an array of the strings ending with an "s" (retaining the order)
+*/
 
 skipTest("collectPlurals() can collect all the strings ending in an s", function () {
-  check(collectPlurals)
-    .whenCalledWith(["dogs", "cat", "apples", "kittens", "kiwi"])
-    .returns(["dogs", "apples", "kittens"]);
-
-  check(collectPlurals)
-    .whenCalledWith(["abcs", "humans", "thoughts", "cloud", "computer", "cups"])
-    .returns(["abcs", "humans", "thoughts", "cups"]);
+  check(collectPlurals(["dogs", "cat", "apples", "kittens", "kiwi"])).isEqualTo(["dogs", "apples", "kittens"]);
+  check(collectPlurals(["abcs", "humans", "thoughts", "cloud", "computer", "cups"])).isEqualTo([
+    "abcs",
+    "humans",
+    "thoughts",
+    "cups",
+  ]);
 });
+
+/*
+createArray()
+
+This function should take a number and a string as its arguments, and return an array of length "number" consisting of elements "string"
+NOTE: you may want to use an array method that isn't a *Higher Order Function* here
+*/
 
 skipTest("createArray() creates an array of the specified length using a specified character", function () {
-  check(createArray).whenCalledWith(3, "!").returns(["!", "!", "!"]);
-  check(createArray).whenCalledWith(5, "a").returns(["a", "a", "a", "a", "a"]);
+  check(createArray(3, "!")).isEqualTo(["!", "!", "!"]);
+  check(createArray(5, "a")).isEqualTo(["a", "a", "a", "a", "a"]);
+  check(createArray(7, "X")).isEqualTo(["X", "X", "X", "X", "X", "X", "X"]);
 });
+
+/*
+deleteManyPasswords()
+
+This function should take an array of objects representing users as an argument
+A user object will take the form:
+{
+  name: "Liam",
+  password: "secret123"
+}
+Your function should return an array of user objects, each with their password property removed
+*/
 
 skipTest("deleteManyPasswords() deletes the password property for each user", function () {
-  check(deleteManyPasswords)
-    .whenCalledWith([
+  check(
+    deleteManyPasswords([
       { name: "Barry", password: "ilovetea" },
       { name: "Sandeep", password: "ilovecoffee" },
       { name: "Kavita", password: "ilovepie" },
     ])
-    .returns([{ name: "Barry" }, { name: "Sandeep" }, { name: "Kavita" }]);
+  ).isEqualTo([{ name: "Barry" }, { name: "Sandeep" }, { name: "Kavita" }]);
 });
 
+/*
+countTheObjects()
+
+This function should take an array containing various data types as an argument, and return a count of how many elements are objects
+*/
+
 skipTest("countTheObjects() counts all the objects in an array of multi-type items", function () {
-  check(countTheObjects)
-    .whenCalledWith([
+  check(
+    countTheObjects([
       { name: "Barry", password: "ilovetea" },
       { name: "Sandeep", password: "ilovecoffee" },
       { name: "Kavita", password: "ilovepie" },
     ])
-    .returns(3);
-  check(countTheObjects).whenCalledWith([{}, {}, {}, {}, {}]).returns(5);
-  check(countTheObjects).whenCalledWith([1, {}, true, {}, {}, false, {}, {}]).returns(5);
-  check(countTheObjects)
-    .whenCalledWith([1, { cat: "Schnitzel" }, true, {}, {}, false, 42, {}, {}])
-    .returns(5);
+  ).isEqualTo(3);
+  check(countTheObjects([{}, {}, {}, {}, {}])).isEqualTo(5);
+  check(countTheObjects([1, {}, true, {}, {}, false, {}, {}])).isEqualTo(5);
+  check(countTheObjects([1, { cat: "Schnitzel" }, true, {}, {}, false, 42, {}, {}])).isEqualTo(5);
+  check(countTheObjects([7, "hello", 45, tru])).isEqualTo(0);
 });
+
+/*
+getEmptyTills()
+
+This function should take an array of objects representing tills as an argument
+A till object will take the form:
+{
+  name: "Dave",
+  checkout: ["haribo", "coffee"]
+}
+Your function should return an array of the till objects that have no elements in their checkout array
+*/
 
 skipTest("getEmptyTills() gets all the tills that are empty", function () {
   const tills = [
