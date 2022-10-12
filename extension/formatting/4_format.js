3277aa20e6d46221609fb45c8954b89bd4e443f0
Liam310 <liam.duncan310@gmail.com>
2022-10-12 09:59:10 +0100

    make formatting worse

--- extension/formatting/4_format.js
+++ extension/formatting/4_format.js
@@ -1,5 +1,7 @@
-if (x + y > 100) {
-  console.log("x + y is bigger than 100");
-} else {
-  console.log("sum is less than or equal to 100");
-}
+if (x+y > 100) {console.log("x + y is bigger than 100");
+} 
+
+
+
+else 
+{ console.log("sum is less than or equal to 100"); }
