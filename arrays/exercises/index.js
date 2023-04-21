1d0b10755d3bf2e11c5b4c08f7babb78b695387c
scar1377 <scarlettadams77@gmail.com>
2023-04-21 16:14:00 +0100

    complete arrays/exercises

--- arrays/exercises/index.js
+++ arrays/exercises/index.js
@@ -12,54 +12,54 @@ e.g.  check(FILL_ME_IN).isEqualTo(2 + 2) becomes -> check(4).isEqualTo(2 + 2)
 runTest("checking multiTypeArray", function () {
   const multiTypeArray = ["I am a string", 42, true, [1, 2, 3]];
 
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[0]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[1]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[2]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[3]);
+  check("string").isEqualTo(typeof multiTypeArray[0]);
+  check("number").isEqualTo(typeof multiTypeArray[1]);
+  check("boolean").isEqualTo(typeof multiTypeArray[2]);
+  check("object").isEqualTo(typeof multiTypeArray[3]);
 });
 
-skipTest("checking alphaSample", function () {
+runTest("checking alphaSample", function () {
   const alphaSample = ["a", "b", "c"];
   alphaSample.push("d");
   alphaSample.push("g");
 
-  check(FILL_ME_IN).isEqualTo(alphaSample);
+  check(["a", "b", "c", "d", "g"]).isEqualTo(alphaSample);
 
   const lastItem = alphaSample.pop();
 
-  check(FILL_ME_IN).isEqualTo(lastItem);
-  check(FILL_ME_IN).isEqualTo(alphaSample);
+  check("g").isEqualTo(lastItem);
+  check(["a", "b", "c", "d"]).isEqualTo(alphaSample);
 });
 
-skipTest("working with nested arrays", function () {
+runTest("working with nested arrays", function () {
   const rows = [
     ["a", "b", "c"],
     ["d", "e", "f"],
     ["g", "h", "i"],
   ];
 
-  check(FILL_ME_IN).isEqualTo(rows[0]);
-  check(FILL_ME_IN).isEqualTo(rows[1]);
-  check(FILL_ME_IN).isEqualTo(rows[2]);
+  check(["a", "b", "c"]).isEqualTo(rows[0]);
+  check(["d", "e", "f"]).isEqualTo(rows[1]);
+  check(["g", "h", "i"]).isEqualTo(rows[2]);
 
   const firstRow = rows[0];
   check(firstRow[0]).isEqualTo("a");
-  check(firstRow[FILL_ME_IN]).isEqualTo("b");
+  check(firstRow[1]).isEqualTo("b");
 
-  check(rows[1][FILL_ME_IN]).isEqualTo("e");
-  check(rows[2][FILL_ME_IN]).isEqualTo("g");
-  check(rows[0][FILL_ME_IN]).isEqualTo("c");
+  check(rows[1][1]).isEqualTo("e");
+  check(rows[2][0]).isEqualTo("g");
+  check(rows[0][2]).isEqualTo("c");
 });
 
-skipTest("get the slice of an array", function () {
+runTest("get the slice of an array", function () {
   const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
 
-  check(FILL_ME_IN).isEqualTo(letters.slice(1, 4));
-  check(FILL_ME_IN).isEqualTo(letters.slice(2, 3));
-  check(FILL_ME_IN).isEqualTo(letters.slice(3, 5));
-  check(FILL_ME_IN).isEqualTo(letters.slice(-1));
-  check(FILL_ME_IN).isEqualTo(letters.slice(-2));
-  check(FILL_ME_IN).isEqualTo(letters.slice(0, -4));
+  check(["b", "c", "d"]).isEqualTo(letters.slice(1, 4));
+  check(["c"]).isEqualTo(letters.slice(2, 3));
+  check(["d", "e"]).isEqualTo(letters.slice(3, 5));
+  check(["n"]).isEqualTo(letters.slice(-1));
+  check(["m", "n"]).isEqualTo(letters.slice(-2));
+  check(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]).isEqualTo(letters.slice(0, -4));
 });
 
 // >>>>>>>>>>> DON'T ALTER ANYTHING BELOW THIS LINE <<<<<<<<<<<<<<<
